cef081556ae4b7a913ad1a59daad31756a76fabc
Robert-Fish <elasys63+github@gmail.com>
2018-12-11 15:45:05 +1000

    added results grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './sass/App.scss';
 import Navbar from './components/layout/Navbar';
+import Results from './components/Results';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
-        <h2>hello</h2>
-      </div>
+      <Results />
     </div>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Results extends Component {
+  constructor() {
+    super();
+    this.state = {
+      courses: [
+        {
+          name: 'React For beginners',
+          image:
+            'https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=codes-coding-computer-270366.jpg&fm=jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, dolorum.',
+        },
+        {
+          name: 'Vue For Beginners',
+          image:
+            'https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=codes-coding-computer-270366.jpg&fm=jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, dolorum.',
+        },
+        {
+          name: 'Angular For beginners',
+          image:
+            'https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=codes-coding-computer-270366.jpg&fm=jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, dolorum.',
+        },
+        {
+          name: 'Express JS For beginners',
+          image:
+            'https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=codes-coding-computer-270366.jpg&fm=jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, dolorum.',
+        },
+        {
+          name: 'Polymer For beginners',
+          image:
+            'https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=codes-coding-computer-270366.jpg&fm=jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, dolorum.',
+        },
+        {
+          name: 'MongoDb For beginners',
+          image:
+            'https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?cs=srgb&dl=codes-coding-computer-270366.jpg&fm=jpg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, dolorum.',
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { courses } = this.state;
+    return (
+      <div className="container">
+        <div className="row">
+          {courses.map(course => (
+            <ItemContent
+              name={course.name}
+              description={course.description}
+              image={course.image}
+              key={course.name}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+const ItemContent = (props) => {
+  const { name, description, image } = props;
+  return (
+    <div className="col-md col-xl-3 col-lg-3 item">
+      <img
+        src={image}
+        alt=""
+        style={{ Height: '5px', Width: '100vw', marginTop: '10px' }}
+        className="img-fluid"
+      />
+      <h3 className="title">{name}</h3>
+      <p className="desc">{description}</p>
+    </div>
+  );
+};
