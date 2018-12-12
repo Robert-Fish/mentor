84b58d1e56d79900b1650f882e0bd1ad69f380bb
Robert-Fish <elasys63+github@gmail.com>
2018-12-12 10:13:00 +1000

    added heroku fix

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(
   })
 );
 
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
